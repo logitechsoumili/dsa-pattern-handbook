a6e14a813e035442191b7db0050706828f1ed5b2
Soumili Saha <logitechsoumili@gmail.com>
2026-07-01 14:57:36 +0530

    C++: 1004. Max Consecutive Ones III

---
+++ 02_Sliding_Window/07_max_consecutive_ones_III/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int zeroCount = 0, left = 0, maxLen = 0;
+
+        for (int right = 0; right < nums.size(); right++){
+            if (nums[right] == 0) zeroCount++;
+
+            while (zeroCount > k){
+                if (nums[left] == 0) zeroCount--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
